d1e5bacefe617cc750169b411089d072aad2aa1f
unknown <cadhage@gmail.com>
2023-04-28 01:11:28 +0530

    adding dynamic hidding technique

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,37 +1,45 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const linksStyle = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  };
+  const toggleLinks = () => {
+    // console.log(linksRef.current.getBoundingClientRect()); //venila js
+    setShowLinks(!showLinks);
+  };
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" className="logo" />
-          <button
-            className="nav-toggle"
-            onClick={() => {
-              setShowLinks(!showLinks);
-            }}
-          >
+          <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className="links-container">
-            <ul className="links">
-              {links.map((link) => {
-                const { id, url, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linksStyle}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   );
